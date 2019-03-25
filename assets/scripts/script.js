326edd8d2db683f00db060d93fa6a53e05457e7e
vinutkumar <vinutkumr.kubsad@accionlabs.com>
2019-03-25 13:28:32 +0530

    files deleted

--- assets/scripts/script.js
+++
@@ -1,6 +0,0 @@
-function underLine(anchor){
-  var spans = document.getElementsByTagName('li');
-   for(var i=0;i<spans.length;i++){
-       spans[i].className = '';
-   }   anchor.parentNode.className = 'active';
- }
